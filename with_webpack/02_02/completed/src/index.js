3ed2fb8712675c7a0df0433f6930ff27c33ea88f
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-04-21 13:15:53 +0200

    update

---
+++ with_webpack/02_02/completed/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const MyComponent = () => <h1>Hello World</h1>
+
+ReactDOM.render(<MyComponent />, document.getElementById('react-container'))
